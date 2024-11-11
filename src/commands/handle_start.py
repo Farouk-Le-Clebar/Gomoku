3cb54a4e475dcfa079149b96e3ae483e435341e6
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-11-11 13:34:15 +0100

    ADD: start command

--- src/commands/handle_start.py
+++ src/commands/handle_start.py
@@ -11,4 +11,6 @@ def handle_start(msg: str) -> None:
         print(" unsupported size or other error")
         return
     protocol.gameBoard = [[0 for _ in range(20)] for _ in range(20)]
+    protocol.rows = 20
+    protocol.cols = 20
     print("OK")
