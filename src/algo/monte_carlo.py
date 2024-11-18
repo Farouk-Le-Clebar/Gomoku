4356ea9590fb041d33450b63e1048d6c62555c09
Louis Huguet <louis.huguet@epitech.eu>
2024-11-18 22:55:56 +0100

    ADD : monte carlo with a preselected area

--- src/algo/monte_carlo.py
+++ src/algo/monte_carlo.py
@@ -3,27 +3,42 @@ import random
 from algo.find_empty_position import find_empty_position 
 from algo.check_simulation import check_event_simulation
 from algo.obvious_play import find_best_move
-from algo.check_simulation import check_event_simulation
+from algo.define_best_area_to_play import define_best_tale_to_play
 
 def monte_carlo():
-    empty_positions = find_empty_position(protocol.gameBoard)
     best = find_best_move(protocol.gameBoard)
-    if (best != None):
+    best_score = [float('inf'), 0, 0]
+    list_score = []
+    
+    if best is not None:
         print(f"{best[0]},{best[1]}")
         protocol.gameBoard[best[0]][best[1]] = 1
-        return
     else:
-        print(f"{empty_positions[0][0]},{empty_positions[0][1]}")
-        protocol.gameBoard[empty_positions[0][0]][empty_positions[0][1]] = 1
-        return
+        empty_positions = define_best_tale_to_play(protocol.gameBoard)
+        for i in range(len(empty_positions)):
+            x, y = empty_positions[i]
+            copyBoard = [row[:] for row in protocol.gameBoard]
+            copyBoard[x][y] = 1
+            score = simulate(copyBoard, 1, 2, 0)
+            list_score.append([score, x, y])
 
-#     empty_positions = find_empty_position(protocol.gameBoard)
-#     for i in range(len(empty_positions)):
-#         x, y = empty_positions[i]
-#         copyBoard = protocol.gameBoard
-#         copyBoard[x][y] = 1
+        for score, x, y in list_score:
+            if score < best_score[0]:
+                best_score = [score, x, y]
+        
+        print(f"DEBUG result : {best_score}")
+        print(f"{best_score[1]},{best_score[2]}")
+        protocol.gameBoard[best_score[1]][best_score[2]] = 1
 
-# def simulate(board, profondeur, player, score):
-#     if (profondeur == 0):
-#         return
-#     empty_positions = find_empty_positions(board)
+def simulate(board, profondeur, player, score) -> int:
+    if profondeur == 0:
+        return score
+    empty_positions = find_empty_position(board)
+    randTale = random.choice(empty_positions)
+    board[randTale[0]][randTale[1]] = player
+    
+    if check_event_simulation(board) == 1:
+        print("DEBUG Simulation ends: event detected")
+        return score
+    score = simulate(board, profondeur - 1, 3 - player, score)
+    return score
