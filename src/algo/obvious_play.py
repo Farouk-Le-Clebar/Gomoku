5c5b33626cdb926aebe076a053634b602ba7c53c
Garoverr <jacques.sapin@epitech.eu>
2024-11-18 15:21:24 +0100

    FIX: the returned best move is only one move

--- src/algo/obvious_play.py
+++ src/algo/obvious_play.py
@@ -7,41 +7,42 @@ def find_best_move(board):
 # Étape 1 : Compléter un alignement direct de 5 pour P1
     move = find_align(board, search_len=4, player=1)
     if move:
-        return move
+        return move[0]
 
 # Étape 1.2 : Compléter un alignement à trou de 5 pour P1
     move = find_gap_completion(board, search_len=4, player=1)
     if move:
-        return move
+        return move[0]
 
 # Étape 2 : Bloquer un alignement direct de 5 pour P2 (adverse)
     move = find_align(board, search_len=4, player=2)
     if move:
-        return move
+        return move[0]
     
 # Étape 2.2 : Bloquer un alignement à trou de 5 pour P2 (adverse) 
     move = find_gap_completion(board, search_len=4, player=2)
     if move:
-        return move
+        return move[0]
 
 # Étape 3 : Bloquer un alignement direct de 4 pour P2 (adverse)
     move = find_align(board, search_len=3, player=2)
     if move:
-        return move
+        return move[0]
 
 # Étape 3.2 : Bloquer un alignement à trou de 4 pour P2 (adverse)
     move = find_gap_completion(board, search_len=3, player=2)
     if move:
-        return move
+        return move[0]
 
 # Étape 4 : Compléter un alignement de 4 pour P1
     move = find_align(board, search_len=3, player=1)
     if move:
-        return move
+        return move[0]
 
 # Étape 4.2 : Compléter un alignement à trou de 4 pour P1
     move = find_gap_completion(board, search_len=3, player=1)
     if move:
-        return move
+        return move[0]
+    
     return None
 
