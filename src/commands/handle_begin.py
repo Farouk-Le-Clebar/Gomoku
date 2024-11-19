7164ce268c17271fa4729125d34ffe24d1e3d86e
Louis Huguet <louis.huguet@epitech.eu>
2024-11-19 11:54:16 +0100

    UPDATE : print coordinate on turn function rather than monte carlo algo ADD : random begin placement

--- src/commands/handle_begin.py
+++ src/commands/handle_begin.py
@@ -1,13 +1,13 @@
 import protocol
+import random
 
 def handle_begin(msg: str) -> None:
     if ((protocol.gameBoard is None) or (protocol.rows == 0 or protocol.cols == 0)):
         print("ERROR message - Game board is not initialized.")
         return
 
-    x = 2
-    y = 17
+    x = random.choice(range(protocol.rows))
+    y = random.choice(range(protocol.cols))
 
     protocol.gameBoard[x][y] = 1
     print(f"{x},{y}")
-
